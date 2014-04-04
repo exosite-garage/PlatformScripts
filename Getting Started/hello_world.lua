f213b6349759816ab67e9c9cfce728cbc7ff593b
Patrick Barrett <PatrickBarrett@Exosite.com>
2014-04-04 13:38:15 -0500

    Use dataport instead of datasource

--- Getting Started/hello_world.lua
+++ Getting Started/hello_world.lua
@@ -1,19 +1,19 @@
 --[[ This is a script that doesn't do anything useful, but that simply show the
 	 general format of platform scripts.
 
-     This script waits on a datasource named 'test_datasource' and prints a
-     debug message with anything that is written to that datasource.
+     This script waits on a dataport named 'test_dataport' and prints a
+     debug message with anything that is written to that dataport.
   ]]
 
-local datasource = alias['Test']
+local dataport = alias['Test']
 
 while true do
-	-- This function waits until a new value is written to the datasource
+	-- This function waits until a new value is written to the dataport
 	-- and returns the timestamp of the datapoint.
-	local datapoint_timestamp = datasource.wait()
+	local datapoint_timestamp = dataport.wait()
 
 	-- Get the datapoint.
-	local datapoint = datasource[datapoint_timestamp]
+	local datapoint = dataport[datapoint_timestamp]
 
 	-- Print it to the debug log.
 	debug(string.format("Got Datapoint: %s", datapoint))
